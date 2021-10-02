263e572b52a1b792ef268b23767ff3247535137e
Kurt <kurtvarcoe@gmail.com>
2021-10-02 17:29:08 +0200

    get access to api data

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
        document.getElementById("demo").innerHTML = xhttp.responseText;
+       var imageData = JSON.parse(xhttp.responseText);
+       console.log(imageData);
     }
 };
-xhttp.open("GET", "https://api.pexels.com/v1/", true);
+xhttp.open("GET", "https://api.pexels.com/v1/search?query=nature&per_page=1", true);
+xhttp.setRequestHeader('Authorization', "563492ad6f917000010000015b4a0ac1e77c42fc8b9eae29d9c26f5a");
 xhttp.send();
