b10cb1e5f82d3ca2132b98a403cf5e927f32a156
Kurt <kurtvarcoe@gmail.com>
2021-10-05 21:42:20 +0200

    fixed html and get request issues

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var searchForm = document.getElementById('search-form');
                 //    document.getElementById("demo").innerHTML = xhttp.responseText;
                 var imageData = JSON.parse(xhttp.responseText);
                 console.log(imageData);
-                var container = document.getElementById("demo");
+                // var container = document.getElementById("demo");
                 var columnOne = document.getElementById('column-one');
                 var columnTwo = document.getElementById('column-two');
                 var columnThree = document.getElementById('column-three');
                 var columnFour = document.getElementById('column-four');
-                container.innerHTML = '';
+                // container.innerHTML = '';
                 columnOne.innerHTML = '';
                 columnTwo.innerHTML = '';
                 columnThree.innerHTML = '';
@@ -34,7 +34,7 @@ var searchForm = document.getElementById('search-form');
             }
         };
         var userInput = document.getElementById('search-bar').value;
-        xhttp.open("GET", `https://api.pexels.com/v1/search?query=${userInput}per_page=8`, true);
+        xhttp.open("GET", `https://api.pexels.com/v1/search?query=${userInput}&per_page=10`, true);
         xhttp.setRequestHeader('Authorization', "563492ad6f917000010000015b4a0ac1e77c42fc8b9eae29d9c26f5a");
         xhttp.send();
        })
